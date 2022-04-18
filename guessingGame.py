2b39e9c7530e04eb0cb378d9d64e303b45a929a4
vibhudiwan08 <82050363+vibhudiwan08@users.noreply.github.com>
2022-04-18 17:47:32 +0530

    Add files via upload

---
+++ guessingGame.py
@@ -0,0 +1,23 @@
+import random
+rand=random.randint(1,9)
+chance=0
+
+while chance<5 :
+
+    guess= int(input("Enter a numner between 0 to 10"))
+
+
+    if (guess == rand):
+        print("You Won")
+        break
+
+    elif (guess<rand):
+        print("Your number is small, guess higher than :", guess)
+
+    else :
+        print("Your guess is big, guess smaller than :",guess)
+
+    chance+=1
+        
+    if not chance<5:
+        print("You lose", "the number is:", rand)
